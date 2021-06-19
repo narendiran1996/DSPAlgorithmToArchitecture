ced7ebcd5a75b153aa8f66a685370b7ce18ea113
narendiran1996 <narendiran1996@gmail.com>
2021-06-19 10:50:34 +0530

    added doc for DFT Implementation

--- DFTImplementations/DFTUsingVectorDotProduct.m
+++ DFTImplementations/DFTUsingVectorDotProduct.m
@@ -2,16 +2,14 @@ clc;
 clear;
 close all;
 x = [1,3,4,5];
-
 X = zeros(size(x));
-
 N = length(x);
-
 
 kVector = 0 : 1 : N-1;
 nVector = 0 : 1 : N-1;
+nVector = nVector';
 
-Mat = kVector.*nVector';
+Mat = kVector.*nVector;
 ExpMat = exp(-j*Mat*2*pi/N);
 
 X = x * ExpMat;
