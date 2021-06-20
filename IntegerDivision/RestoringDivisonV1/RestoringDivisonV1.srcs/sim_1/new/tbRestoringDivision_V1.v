4237406e45a35a4ba48690d180108f11fdac914c
narendiran1996 <narendiran1996@gmail.com>
2021-06-20 06:42:49 +0530

    added integer division using restoring method

---
+++ IntegerDivision/RestoringDivisonV1/RestoringDivisonV1.srcs/sim_1/new/tbRestoringDivision_V1.v
@@ -0,0 +1,75 @@
+`timescale 1ns / 1ps
+
+module tbRestoringDivision_V1();
+
+localparam N = 32;
+
+reg clk, rst, start;
+reg [N-1:0]Dividend, Divisor;
+wire [N-1:0]Quotient;
+wire done;
+
+
+RestoringDivisionV1
+                    #
+                    (
+                        .N(N)
+                    )
+                    DUT
+                    (
+                        clk,
+                        rst,
+                        start,
+                        Dividend,
+                        Divisor,
+                        Quotient,
+                        done
+                    );
+
+
+always
+    begin
+        clk = 0;
+        #5;
+        clk = 1;
+        #5;
+    end
+
+integer i;
+integer Qval;
+
+    
+ 
+initial
+    begin
+        rst = 1;
+        start = 0;
+        Dividend = 0;
+        Divisor = 0;
+        #143
+        rst = 0;
+        #76;
+
+        for(i=0;i<1000;i=i+1)
+            begin
+                @(posedge clk);
+                Dividend = $urandom%256;
+                Divisor = 1+($urandom%255);
+                Qval = Dividend / Divisor;
+                start = 1;
+                @(posedge clk);
+                start = 0;
+                wait(done==1);
+                
+                if(Qval != Quotient) 
+                    begin
+                        $error("It's gone wrong");
+                        $finish;
+                    end
+            end
+        $finish;
+        
+    end
+
+endmodule
+
